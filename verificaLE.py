520cab78f29b25ef03046560d13a403917005415
Jeffiemax <61049424+Jeffiemax@users.noreply.github.com>
2022-01-25 22:04:13 -0300

    Add files via upload

---
+++ verificaLE.py
@@ -0,0 +1,285 @@
+import verificaLE
+le = verificaLE.LE()
+O leitor de id 1 iniciou
+O escritor de id 1 iniciou
+O escritor de id 4 iniciou
+O escritor de id 3 iniciou
+O leitor de id 2 iniciou
+O escritor de id 2 iniciou
+O leitor&escritor de id 1 iniciou
+le.leitorLendo(1)
+O leitor&escritor de id 2 iniciou
+O leito de id 1 está executando
+O 0 eh par
+le.leitorSaindo(1)
+le.leitorescritorLendo&Escrevendo(2)
+O leito&escritor de id 2 está executando
+O valor da variável compartilhada é 0
+O valor da variável foi alterada para 1
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 2
+le.escritorSaindo(2)
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
+le.escritorEscrevendo(3)
+O escritor de id 3 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 3
+le.escritorSaindo(3)
+le.escritorEscrevendo(4)
+O escritor de id 4 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 4
+le.escritorSaindo(4)
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 4
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(2)
+le.leitorescritorSaindo(1)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 1 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+O leitor&escritor de id 1 iniciou
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 2 eh par
+le.leitorSaindo(1)
+O escritor de id 2 iniciou
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 2
+le.escritorSaindo(2)
+O leitor de id 2 iniciou
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(1)
+O leitor&escritor de id 2 iniciou
+le.leitorescritorLendo&Escrevendo(2)
+O leito&escritor de id 2 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 2 eh par
+le.leitorSaindo(1)
+O escritor de id 3 iniciou
+le.escritorEscrevendo(3)
+O escritor de id 3 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 3
+le.escritorSaindo(3)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(2)
+O leitor&escritor de id 1 iniciou
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 2 eh par
+le.leitorSaindo(1)
+O escritor de id 2 iniciou
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 2
+le.escritorSaindo(2)
+O leitor de id 2 iniciou
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
+O escritor de id 4 iniciou
+le.escritorEscrevendo(4)
+O escritor de id 4 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 4
+le.escritorSaindo(4)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 4
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(1)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 1 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+O leitor&escritor de id 1 iniciou
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor&escritor de id 2 iniciou
+le.leitorescritorLendo&Escrevendo(2)
+O leito&escritor de id 2 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 3
+le.leitorescritorSaindo(1)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 3 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 2 iniciou
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 2
+le.escritorSaindo(2)
+O leitor de id 2 iniciou
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
+O escritor de id 3 iniciou
+le.escritorEscrevendo(3)
+O escritor de id 3 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 3
+le.escritorSaindo(3)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(2)
+O leitor&escritor de id 1 iniciou
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 2 eh par
+le.leitorSaindo(1)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(1)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 1 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 2 iniciou
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+le.escritorSaindo(2)
+O leitor de id 2 iniciou
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
+O escritor de id 4 iniciou
+le.escritorEscrevendo(4)
+O escritor de id 4 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 4
+le.escritorSaindo(4)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 4
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+O leitor&escritor de id 2 iniciou
+le.leitorescritorLendo&Escrevendo(2)
+O leito&escritor de id 2 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor&escritor de id 1 iniciou
+le.leitorescritorLendo&Escrevendo(1)
+O leito&escritor de id 1 está executando
+O valor da variável compartilhada é 2
+O valor da variável foi alterada para 3
+le.leitorescritorSaindo(2)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 3 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 3 iniciou
+le.escritorEscrevendo(3)
+O escritor de id 3 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 3
+le.escritorSaindo(3)
+O escritor de id 1 iniciou
+le.escritorEscrevendo(1)
+O escritor de id 1 está executando
+O valor da variável compartilhada é 3
+O valor da variável foi alterada para 1
+le.escritorSaindo(1)
+le.leitorescritorSaindo(1)
+O leitor de id 1 iniciou
+le.leitorLendo(1)
+O leito de id 1 está executando
+O 1 eh ímpar
+le.leitorSaindo(1)
+O escritor de id 2 iniciou
+le.escritorEscrevendo(2)
+O escritor de id 2 está executando
+O valor da variável compartilhada é 1
+O valor da variável foi alterada para 2
+O leitor de id 2 iniciou
+le.escritorSaindo(2)
+le.leitorLendo(2)
+O leito de id 2 está executando
+O 2 eh par
+le.leitorSaindo(2)
